681f73844af05f7ded429a711d3d61043e5f1351
centerprime <Happy0990!@#>
2020-11-23 21:27:03 +0900

    - minor changes

--- app/src/main/java/com/example/centerprimesampleqkcsdk/SendQKCActivity.java
+++ app/src/main/java/com/example/centerprimesampleqkcsdk/SendQKCActivity.java
@@ -2,6 +2,7 @@ package com.example.centerprimesampleqkcsdk;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,9 @@ import com.example.centerprimesampleqkcsdk.databinding.ActivitySendQkcBinding;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class SendQKCActivity extends AppCompatActivity {
 
@@ -35,18 +39,32 @@ public class SendQKCActivity extends AppCompatActivity {
 
 
         binding.sendBNB.setOnClickListener(v -> {
-            if (!TextUtils.isEmpty(binding.address.getText().toString().trim()) && !TextUtils.isEmpty(binding.ethAmount.getText().toString().trim())
+            if (!TextUtils.isEmpty(binding.qkcAddress.getText().toString().trim()) && !TextUtils.isEmpty(binding.ethAddress.getText().toString().trim()) && !TextUtils.isEmpty(binding.ethAmount.getText().toString().trim())
                     && !TextUtils.isEmpty(binding.gasLimit.getText().toString().trim())
                     && !TextUtils.isEmpty(binding.receiverAddress.getText().toString().trim())
                     && !TextUtils.isEmpty(binding.password.getText().toString().trim())) {
 
-                String walletAddress = binding.address.getText().toString();
+                String walletAddress = binding.qkcAddress.getText().toString();
+                String ethAddress = binding.ethAddress.getText().toString();
                 String password = binding.password.getText().toString();
                 //    BigInteger gasPrice = new BigInteger(String.valueOf(ethManager.getGasPrice()));
                 BigInteger gasPrice = new BigInteger("30000000000");
                 BigInteger gasLimit = new BigInteger(binding.gasLimit.getText().toString());
-                BigDecimal etherAmount = new BigDecimal(binding.ethAmount.getText().toString().trim());
+                BigDecimal qkcAmount = new BigDecimal(binding.ethAmount.getText().toString().trim());
                 String receiverAddress = binding.receiverAddress.getText().toString().trim();
+
+
+                qkcManager.sendQCKToken(ethAddress, walletAddress, password, walletAddress, receiverAddress,gasPrice, gasLimit, qkcAmount.toString(), this)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(tx -> {
+
+                            Toast.makeText(this, "TX : " + tx, Toast.LENGTH_SHORT).show();
+
+                        }, error -> {
+                            Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        });
 /*
                 binanceManager.sendQCKToken(walletAddress, password, gasPrice, gasLimit, etherAmount, receiverAddress, this)
                         .subscribeOn(Schedulers.io())
